d422c2414fe8bb92ae6c62d5a594a157f7d61373
Math649i <math649i@gmail.com>
2026-02-03 11:37:33 +0100

    Github action til python unit test

--- test/test_examples.py
+++ test/test_examples.py
@@ -21,3 +21,23 @@ def test_crash():
     raise RuntimeError("Test crashede med vilje")
 
     assert False # failed test bliver ignoreret
+
+
+def test_addition():
+   
+    assert 2 + 3 == 5
+
+def test_string_length():
+    
+    assert len("hej") == 3
+
+
+def test_subtraction():
+   
+    assert 10 - 4 == 5
+
+
+def test_list_contains():
+    
+    numbers = [1, 2, 3]
+    assert 4 in numbers
